6a1b7dc81409c6fb16fee3eec761b12cc50566ff
Darshendran <darshendran@gmail.com>
2021-10-29 22:22:22 +0800

    1,2,3.5.75 (ccp & Java & PHP -DONE)

--- app/src/main/java/com/example/groupb17/php_Learn.java
+++ app/src/main/java/com/example/groupb17/php_Learn.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class php_Learn extends AppCompatActivity {
     ImageButton back_learn_btn;
+    TextView python_subtopic_1, python_subtopic_2, python_subtopic_3, python_subtopic_4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,46 @@ public class php_Learn extends AppCompatActivity {
                 finish();
             }
         });
+
+        python_subtopic_1 = findViewById(R.id.python_subtopic_1);
+        python_subtopic_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent python_subtopic_1 = new Intent(php_Learn.this, php_sub1.class);
+                startActivity(python_subtopic_1);
+                finish();
+            }
+        });
+        python_subtopic_2 = findViewById(R.id.python_subtopic_2);
+        python_subtopic_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent python_subtopic_2 = new Intent(php_Learn.this, php_sub2.class);
+                startActivity(python_subtopic_2);
+                finish();
+            }
+        });
+        python_subtopic_3 = findViewById(R.id.python_subtopic_3);
+        python_subtopic_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent python_subtopic_3 = new Intent(php_Learn.this, php_sub3.class);
+                startActivity(python_subtopic_3);
+                finish();
+            }
+        });
+        python_subtopic_4 = findViewById(R.id.python_subtopic_4);
+        python_subtopic_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent java_subtopic_4 = new Intent(php_Learn.this, php_sub4.class);
+                startActivity(java_subtopic_4);
+                finish();
+            }
+        });
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/groupb17/php_sub3.java
@@ -0,0 +1,31 @@
+package com.example.groupb17;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class php_sub3 extends AppCompatActivity {
+    ImageButton back_learn_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.php_sub3);
+
+        back_learn_btn = findViewById(R.id.back_learn_btn);
+        back_learn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backLearnBtn2 = new Intent(php_sub3.this, php_Learn.class);
+                startActivity(backLearnBtn2);
+                finish();
+            }
+        });
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
