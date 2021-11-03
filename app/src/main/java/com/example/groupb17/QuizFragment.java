0b3e1de894790a1acf6184436388ca692ecb31c8
Darshendran <darshendran@gmail.com>
2021-11-03 19:55:40 +0800

    1,2,3,4.90 [QnA for cpp, php and phy only left]

--- app/src/main/java/com/example/groupb17/QuizFragment.java
+++ app/src/main/java/com/example/groupb17/QuizFragment.java
@@ -19,7 +19,16 @@ public class QuizFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_quiz, container, false);
 
-
+        //Intent to cpp Quiz
+        Button cppQuizBtn = (Button) view.findViewById(R.id.cppQuizBtn);
+        cppQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "C++ Quiz Selected", Toast.LENGTH_SHORT).show();
+                Intent cppQuizBtn = new Intent(getActivity(),quiz_cpp.class);
+                startActivity(cppQuizBtn);
+            }
+        });
         //Intent to Java Quiz
         Button javaQuizBtn = (Button) view.findViewById(R.id.javaQuizBtn);
         javaQuizBtn.setOnClickListener(new View.OnClickListener() {
@@ -30,7 +39,26 @@ public class QuizFragment extends Fragment {
                 startActivity(javaQuizBtn);
             }
         });
-
+        //Intent to php Quiz
+        Button phpQuizBtn = (Button) view.findViewById(R.id.phpQuizBtn);
+        phpQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Php Quiz Selected", Toast.LENGTH_SHORT).show();
+                Intent phpQuizBtn = new Intent(getActivity(),quiz_php.class);
+                startActivity(phpQuizBtn);
+            }
+        });
+        //Intent to Python Quiz
+        Button pythonQuizBtn = (Button) view.findViewById(R.id.pythonQuizBtn);
+        pythonQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Python Quiz Selected", Toast.LENGTH_SHORT).show();
+                Intent pythonQuizBtn = new Intent(getActivity(),quiz_python.class);
+                startActivity(pythonQuizBtn);
+            }
+        });
 
         return view;
     }
